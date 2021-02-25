4749a74bb14940f08429cb91c15fec2f6a93ac93
hobeol <holger.behmann@gmail.com>
2021-02-25 01:06:09 +0100

    little fixes

--- Test/Unit/PSPath.Tests.ps1
+++ Test/Unit/PSPath.Tests.ps1
@@ -1,14 +1,13 @@
-BeforeAll {
-    $ModulePath = Resolve-Path "$PSScriptRoot\..\.."
-    $ModuleSourcePath = Resolve-Path "$ModulePath\Source"
-    $ModuleName = (Get-Item $ModulePath).Name
-    $ModuleManifestName = 'PSPath.psd1'
-    $ModuleManifestPath = Join-Path -Path $ModuleSourcePath -ChildPath $ModuleManifestName
-    #Write-Host ("Module:`n`tPath:`t{0}`n`tSourcePath:`t{1}`n`tName:`t{2}`n`tManifestName:`t{3}`n`tManifestPath:`t{4}" -f
-    #    $ModulePath,$ModuleSourcePath,$ModuleName,$ModuleManifestName,$ModuleManifestPath)
-}
-
 Describe 'Core Module Tests' -Tags 'CoreModule', 'Unit' {
+    BeforeAll {
+        $ModulePath = Resolve-Path "$PSScriptRoot\..\.."
+        $ModuleSourcePath = Resolve-Path "$ModulePath\Source"
+        $ModuleName = (Get-Item $ModulePath).Name
+        $ModuleManifestName = 'PSPath.psd1'
+        $ModuleManifestPath = Join-Path -Path $ModuleSourcePath -ChildPath $ModuleManifestName
+        Write-Host ("Module:`n`tPath:`t{0}`n`tSourcePath:`t{1}`n`tName:`t{2}`n`tManifestName:`t{3}`n`tManifestPath:`t{4}" -f
+            $ModulePath,$ModuleSourcePath,$ModuleName,$ModuleManifestName,$ModuleManifestPath)
+    }
 
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath
